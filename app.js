03ed8c05f6997ec7afc55266911095dabe573e93
Artyom Lodochnikov <lodkin55@yandex.ru>
2021-05-04 13:27:11 +0500

    update app.js

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const { dataBaseUrl, dataBaseOptions } = require('./utils');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const router = require('./routers');
+const limiter = require('./middlewares/limiter');
+const crossOrigin = require('./middlewares/cors')
+const { dataBaseUrl, dataBaseOptions } = require('./configs/databaseConfig');
+const errorHandler = require('./middlewares/error-handler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 try {
 mongoose.connect(dataBaseUrl, dataBaseOptions)
-} catch (error) {
-  console.log(`Ошибка поключения к БД: ${error}`);
+} catch (err) {
+  errorHandler(err);
 }
+
+app.use(limiter);
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(crossOrigin);
+
+app.use(requestLogger);
+app.use(router);
+
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
