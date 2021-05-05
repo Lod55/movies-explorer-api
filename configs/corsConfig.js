a790e6187eb3990018ebef0f348690dd60cbd648
Artyom Lodochnikov <lodkin55@yandex.ru>
2021-05-05 12:34:35 +0500

    update cors config

--- configs/corsConfig.js
+++ configs/corsConfig.js
@@ -1,6 +1,8 @@
 const corsOptions = {
   origin: [
     'http://localhost:3000',
+    'http://app-movies.lod55.nomoredomains.icu',
+    'https://app-movies.lod55.nomoredomains.icu',
   ],
   credentials: true,
 };
